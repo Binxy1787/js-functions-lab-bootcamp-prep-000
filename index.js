0e652d81276df4ce5a53cc3d0fc79ea6a7e94437
Jameson Nelson <nelson_jameson@yahoo.com>
2019-01-24 22:26:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function happyHolidaysTo(name){
 }
 
 function happyHolidayTo(holiday, name){
-  return `Happy ${holiday}, ${you}!`;
+  return `Happy ${holiday}, ${name}!`;
 }
 
 function holidayCountdown(holiday, days){
